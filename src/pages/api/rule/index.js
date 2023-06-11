6bb201367c830ea868872ab618ce9812f0dee75d
Rizky <rizkymuhammad416@gmail.com>
2023-06-11 21:34:42 +0700

    add api count for summart and transaction

--- src/pages/api/rule/index.js
+++ src/pages/api/rule/index.js
@@ -1,4 +1,5 @@
 import { pool } from "@/config/mysql-conn";
+import { isEmpty } from "lodash";
 
 export default async function handler(req, res) {
   switch (req.method) {
@@ -12,6 +13,7 @@ export default async function handler(req, res) {
 const saveRule = async (req, res) => {
   try {
     const { confidence } = req.body;
+    if (isEmpty(confidence)) return res.status(400).send("Tidak ada items yang memenuhi minimal support");
     const sql = "INSERT INTO rule (summary_id, rule, confidence, lift, description) VALUES ?"
     const result = await pool.query(sql, [confidence]);
     return res.status(200).json(result);
